59d93a219baff8c3b2a6e4a228eddce440b6b88a
Johnson <johnsonphan95@gmail.com>
2019-12-18 01:56:19 -0800

    Display block transations, account, and balance

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import './App.css';
 
 function App() {
 
-  const [account, setAccount] = useState("");
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [block, setBlock] = useState(null);
 
   useEffect(() => {
     loadBlockchainData()
-  }, [])
+  }, [account, balance])
 
   const loadBlockchainData = async () => {
+    console.log('called')
     let web3Provider;
     if (window.ethereum) {
       web3Provider = window.ethereum;
@@ -30,12 +33,27 @@ function App() {
     const web3 = new Web3(web3Provider);
     const accounts = await web3.eth.getAccounts();
     setAccount(accounts[0]);
+    if (account) {
+      const wei = await web3.eth.getBalance(account);
+      setBalance(web3.utils.fromWei(wei));
+    }
+    const data = await web3.eth.getBlock();
+    setBlock(data.transactions); 
+    
   }
 
   return (
     <div className="App">
     <h1>Hello World!</h1>
-    {account}
+    <h1>address: {account}</h1> 
+    <h1>balance: {balance}</h1>
+    <ul>
+      {block ? block.map((transaction) => (
+        <li>
+          {transaction}
+        </li>
+      )) : null}
+    </ul>
     </div>
   );
 }
