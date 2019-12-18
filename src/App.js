6b90e0f76f35a46d864d83e78ad3aa0f8e8cc450
Johnson <johnsonphan95@gmail.com>
2019-12-17 18:07:11 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  const loadBlockchainData = async () => {
+    let web3Provider;
+    if (window.ethereum) {
+      web3Provider = window.ethereum;
+      try { 
+        await window.ethereum.enable();
+      } catch (error) {
+          console.log('User denied account access')
+      }
+    } 
+    else if (window.web3) {
+      web3Provider = window.web3.currentProvider;
+    }
+    else {
+      web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    const web3 = new Web3(web3Provider);
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello World!</h1>
+    {account}
     </div>
   );
 }
