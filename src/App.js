a6550c9b1191c232241ca4f037a376cc486c0a48
Johnson <johnsonphan95@gmail.com>
2019-12-18 18:22:30 -0800

    TodoList functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Web3 from 'web3';
 import './App.css';
 
 function App() {
-
   const [account, setAccount] = useState(null);
   const [balance, setBalance] = useState(null);
   const [block, setBlock] = useState(null);
 
   useEffect(() => {
     loadBlockchainData()
-  }, [account, balance])
+  }, [])
 
   const loadBlockchainData = async () => {
     let web3Provider;
@@ -29,16 +27,22 @@ function App() {
     else {
       web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
-    const web3 = new Web3(web3Provider);
-    const accounts = await web3.eth.getAccounts();
+    const web3 = await new Web3(web3Provider);
+    await loadAccount(web3);
+    await loadBlock(web3);
+  }
+
+  const loadAccount = async (web3) => {
+    const accounts = await web3.eth.getAccounts(); 
+    const address = accounts[0];
+    const bal = await web3.eth.getBalance(accounts[0]);
     setAccount(accounts[0]);
-    if (account) {
-      const wei = await web3.eth.getBalance(account);
-      setBalance(web3.utils.fromWei(wei));
-    }
+    setBalance(web3.utils.fromWei(bal));
+  }
+
+  const loadBlock = async (web3) => {
     const data = await web3.eth.getBlock();
     setBlock(data.transactions); 
-    
   }
 
   return (
@@ -47,8 +51,8 @@ function App() {
     <h1>address: {account}</h1> 
     <h1>balance: {balance}</h1>
     <ul>
-      {block ? block.map((transaction) => (
-        <li>
+      {block ? block.map((transaction, i) => (
+        <li key={i}>
           {transaction}
         </li>
       )) : null}
