f244398c4402f194131cde6babe6b0a0773a8b8f
Johnson <johnsonphan95@gmail.com>
2019-12-19 23:06:25 -0800

    Load todoList on mount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config';
 import Web3 from 'web3';
 import './App.css';
 
 function App() {
-  const [account, setAccount] = useState(null);
-  const [balance, setBalance] = useState(null);
-  const [block, setBlock] = useState(null);
+  const [account, setAccount] = useState('');
+  const [balance, setBalance] = useState(0);
+  const [block, setBlock] = useState(0);
+  const [todoList, setTodoList] = useState(0);
+  const [taskCount, setTaskCount] = useState(0);
 
   useEffect(() => {
     loadBlockchainData()
@@ -30,19 +33,27 @@ function App() {
     const web3 = await new Web3(web3Provider);
     await loadAccount(web3);
     await loadBlock(web3);
+    await loadTodoList(web3);
   }
 
   const loadAccount = async (web3) => {
     const accounts = await web3.eth.getAccounts(); 
     const address = accounts[0];
-    const bal = await web3.eth.getBalance(accounts[0]);
-    setAccount(accounts[0]);
+    const bal = await web3.eth.getBalance(address);
+    setAccount(address);
     setBalance(web3.utils.fromWei(bal));
   }
 
   const loadBlock = async (web3) => {
-    const data = await web3.eth.getBlock();
-    setBlock(data.transactions); 
+    const _block = await web3.eth.getBlock();
+    setBlock(_block.transactions); 
+  }
+
+  const loadTodoList = async (web3) => {
+    const _todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
+    setTodoList(_todoList);
+    const _taskCount = await _todoList.methods.taskCount().call();
+    setTaskCount(_taskCount);
   }
 
   return (
@@ -50,6 +61,7 @@ function App() {
     <h1>Hello World!</h1>
     <h1>address: {account}</h1> 
     <h1>balance: {balance}</h1>
+  <h1>Task Count: {taskCount}</h1>
     <ul>
       {block ? block.map((transaction, i) => (
         <li key={i}>
